dab036872271f642eaae32d78aff0085fac7f4ba
jws <350331045@qq.com>
2021-12-22 10:56:47 +0800

    沾喜气

--- index.js
+++ index.js
@@ -60,6 +60,28 @@ const drawFn = async () => {
     return Promise.resolve(`签到成功！恭喜抽到：${draw.data.lottery_name}`);
 };
 
+const xiqi = async () => {
+    const list = await fetch('https://api.juejin.cn/growth_api/v1/lottery_history/global_big', {
+        headers,
+        method: 'POST',
+        credentials: 'include',
+        body: JSON.stringify({ page_no: 1, page_size: 5 }),
+    }).then((res) => res.json());
+
+    const res = await fetch('https://api.juejin.cn/growth_api/v1/lottery_lucky/dip_lucky', {
+        headers,
+        method: 'POST',
+        credentials: 'include',
+        body: JSON.stringify({ lottery_history_id: list.data.lotteries[0].history_id }),
+    }).then((res) => res.json());
+
+    if (res.err_no !== 0) return Promise.reject('网络异常！');
+
+    if (res.data.has_dip) return `今日已经沾过喜气！喜气值：${res.data.total_value}`;
+
+    if (res.data.dip_action === 1) return `沾喜气成功！喜气值：${res.data.total_value}`;
+};
+
 // 梭哈 一次抽完
 const allDarw = async () => {
     if (score < 200) {
@@ -138,12 +160,14 @@ const allDarw = async () => {
         score = res.data;
         // 先去免费抽奖
         let msg = await drawFn();
+        let msgxiqi = await xiqi();
         let award;
         // 是否梭哈
         if (suoha) {
             award = await allDarw();
         }
         return {
+            zxq: msgxiqi,
             meiri: msg,
             suohua: award,
         };
@@ -157,6 +181,7 @@ const allDarw = async () => {
                 content: `
         <h1 style="text-align: center">自动签到通知</h1>
         <p style="text-indent: 2em">签到结果：${data.meiri}</p>
+        <p style="text-indent: 2em">沾喜气结果：${data.zxq}</p>
         <p style="text-indent: 2em">梭哈结果：${JSON.stringify(data.suohua)}</p>
         <p style="text-indent: 2em">当前积分：${score}</p><br/>
         `,
