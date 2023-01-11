b89e718e19dada4f8a5738d0b0ff46115d5cdc3d
lpp <350331045@qq.com>
2023-01-11 22:09:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -128,25 +128,7 @@ const allDarw = async () => {
 
 // 签到
 (async () => {
-    // 查询今日是否已经签到
-    const today_status = await fetch('https://api.juejin.cn/growth_api/v1/get_today_status', {
-        headers,
-        method: 'GET',
-        credentials: 'include',
-    }).then((res) => res.json());
-
-    if (today_status.err_no !== 0) return Promise.reject('签到失败！');
-    if (today_status.data) return Promise.resolve('今日已经签到！');
-
-    // 签到
-    const check_in = await fetch('https://api.juejin.cn/growth_api/v1/check_in', {
-        headers,
-        method: 'POST',
-        credentials: 'include',
-    }).then((res) => res.json());
-
-    if (check_in.err_no !== 0) return Promise.reject('签到异常！');
-    return Promise.resolve(`签到成功！当前积分；${check_in.data.sum_point}`);
+    return Promise.resolve();
 })()
     .then((msg) => {
         console.log(msg);
